e0a648950ea958f6260a193e10e678456ab2c5db
Meier-chen <m_chen27@u.pacific.edu>
2020-10-30 11:25:15 -0700

    card class

--- group-project/src/project/Card.java
+++ group-project/src/project/Card.java
@@ -1,5 +1,17 @@
 package project;
-
 public class Card {
 
+	public int attackcards;
+	public int Equipmentcards;
+	public int actioncards;
+	public int manacards;
+	public int healingcards;	
+	//public string getName;
+	
+	private int cost;
+	
+	public int getattackcards() {
+		return attackcards;
+	}
 }
+
