8f60008df83f4c5c4d3773465f6c0a90148ebdd6
Meier-chen <m_chen27@u.pacific.edu>
2020-10-30 11:05:53 -0700

    shop change

--- group-project/src/project/Shop.java
+++ group-project/src/project/Shop.java
@@ -1,5 +1,21 @@
 package project;
 
-public class Shop {
 
+public class Shop {
+	private int buycards;
+	private int sellcards;
+	private int money;
+	
+	public Shop(int buycards, int sellcards) {
+		this.buycards = buycards;
+		this.sellcards = sellcards;
+	}
+	
+	public int getbuycards() {
+		return buycards;
+	}
+	
+public int getsellcards() {
+	return sellcards;
+}
 }
