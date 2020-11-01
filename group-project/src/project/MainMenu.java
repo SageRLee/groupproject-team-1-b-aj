52d3cde5766603f62a66a7cf922da76b18e4add5
Sage Lee <s_lee146@u.pacific.edu>
2020-10-31 19:45:25 -0700

    Added resolution

--- group-project/src/project/MainMenu.java
+++ group-project/src/project/MainMenu.java
@@ -1,5 +1,8 @@
 package project;
+
 public class MainMenu {
+	public static final int RESOLUTION_X = 1920;
+	public static final int RESOLUTION_Y = 1080;
 	public static void main(String[] args) {
 		
 		
