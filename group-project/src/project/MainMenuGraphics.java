d797607da061b8d34c7d1a9b05529a7bb25143b8
Sage Lee <s_lee146@u.pacific.edu>
2020-10-31 19:13:07 -0700

    Added images folder

--- group-project/src/project/MainMenuGraphics.java
+++ group-project/src/project/MainMenuGraphics.java
@@ -2,13 +2,18 @@ package project;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class MainMenuGraphics {
+public class MainMenuGraphics extends GraphicsProgram {
 	private static GImage titleImage;
 	private static GImage playButtonImage;
 	private static GImage shopButtonImage;
+	private static GImage background = new GImage("group-project/media/images/Background.png", 0, 0);
 	
-
+	public void run() {
+		
+		add(background);
+	}
 	public void mousePressed(MouseEvent e) {
 		//Mouse event for all button options
 	}
