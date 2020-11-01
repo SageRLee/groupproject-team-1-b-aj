40fa29c016a62cc9892d3156e4a8cf77e4e8865c
Sage Lee <s_lee146@u.pacific.edu>
2020-10-31 19:30:50 -0700

    Reorganized media folders

--- group-project/src/project/MainMenuGraphics.java
+++ group-project/src/project/MainMenuGraphics.java
@@ -8,7 +8,7 @@ public class MainMenuGraphics extends GraphicsProgram {
 	private static GImage titleImage;
 	private static GImage playButtonImage;
 	private static GImage shopButtonImage;
-	private static GImage background = new GImage("group-project/media/images/Background.png", 0, 0);
+	private static GImage background = new GImage("media/images/Background.png", 0, 0);
 	
 	public void run() {
 		
