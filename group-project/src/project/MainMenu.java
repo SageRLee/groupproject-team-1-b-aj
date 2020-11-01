f2e6de318b3997ddfe99433e6012d56aa87f1937
Sage Lee <s_lee146@u.pacific.edu>
2020-10-31 20:33:33 -0700

    Added images to menu graphics

--- group-project/src/project/MainMenu.java
+++ group-project/src/project/MainMenu.java
@@ -1,9 +1,10 @@
 package project;
 
 public class MainMenu {
-	
+	public static final int RESOLUTION_X = 1920;
+	public static final int RESOLUTION_Y = 1080;
 	public static void main(String[] args) {
-		
+
 		
 	}
 	
