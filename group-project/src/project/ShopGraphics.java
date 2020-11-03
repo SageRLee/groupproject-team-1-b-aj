32e0b28beeac3fdb08b297ee60ae58ceedc85251
Meier-chen <m_chen27@u.pacific.edu>
2020-11-03 14:29:48 -0800

    shop graphics code change

--- group-project/src/project/ShopGraphics.java
+++ group-project/src/project/ShopGraphics.java
@@ -1,5 +1,25 @@
 package project;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import acm.graphics.GImage;
+import acm.program.GraphicsProgram;
 
-public class ShopGraphics {
+public class ShopGraphics extends ProjectGraphics implements ActionListener {
+		GImage shopbackground;
 
+		
+		public void run() {
+			initializeApplet();
+			shopbackground = new GImage("media/images/shop background.jpg", 0, 0);
+			shopbackground.setSize(ProjectGraphics.RESOLUTION_X, ProjectGraphics.RESOLUTION_Y);
+			add(shopbackground);
+
+			
+		}
+		
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			
+		}
 }
