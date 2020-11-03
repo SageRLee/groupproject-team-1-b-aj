b1fc01b4b000937685f2e3f588649a647489865e
ChengboFan <c_fan2@u.pacific.edu>
2020-11-03 17:01:33 +0800

    create hashmap levelmap to make connect with GIamge class and Level class.(commited to wait level class)

--- group-project/src/project/MapGraphics.java
+++ group-project/src/project/MapGraphics.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.util.HashMap;
 
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
@@ -23,8 +24,42 @@ public class MapGraphics extends ProjectGraphics implements ActionListener {
 	public GImage Level6;
 	public GImage Level7;
 	public GImage Level8;
+	public Level LevelOne;
+	public Level LevelTwo;
+	public Level LevelThree;
+	public Level LevelFlour;
+	public Level LevelFive;
+	public Level LevelSix;
+	public Level LevelSeven;
+	public Level LevelEight;
+	public HashMap<GImage , Level > levelmap;
 	public void run() {
 		Map = new GImage("media/images/Background.png", 0, 0);
+		
+//		Level1=new  GImage("media/images/level1.png", 0, 0);
+//		Level2=new  GImage("media/images/level2.png", 0, 0);
+//		Level3=new  GImage("media/images/level3.png", 0, 0);
+//		Level4=new  GImage("media/images/level4.png", 0, 0);
+//		Level5=new  GImage("media/images/level5.png", 0, 0);
+//		Level6=new  GImage("media/images/level6.png", 0, 0);
+//		Level7=new  GImage("media/images/level7.png", 0, 0);
+//		Level8=new  GImage("media/images/level8.png", 0, 0);
+//		levelmap.put(Level1,LevelOne);
+//		levelmap.put(Level2,LevelTwo);
+//		levelmap.put(Level3,LevelThree);
+//		levelmap.put(Level4,LevelFlour);
+//		levelmap.put(Level5,LevelFive);
+//		levelmap.put(Level6,LevelSix);
+//		levelmap.put(Level7,LevelSeven);
+//		levelmap.put(Level8,LevelEight);
 		add(Map);
+//		add(Level1);
+//		add(Level2);
+//		add(Level3);
+//		add(Level4);
+//		add(Level5);
+//		add(Level6);
+//		add(Level7);
+//		add(Level8);
 	}
 }
