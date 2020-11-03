3f12ec36a23ed2ab6595837f8966c62ca581902f
ChengboFan <c_fan2@u.pacific.edu>
2020-11-03 16:33:37 +0800

    add background in MapGraphics(staegenumber.png will be upload soon)

--- group-project/src/project/MapGraphics.java
+++ group-project/src/project/MapGraphics.java
@@ -1,5 +1,30 @@
 package project;
 
-public class MapGraphics {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.graphics.GLine;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+public class MapGraphics extends ProjectGraphics implements ActionListener {
+	public static final int RESOLUTION_X = 1920;
+	public static final int RESOLUTION_Y = 1080;
+	private GImage Map;
+	public GImage Level1;
+	public GImage Level2;
+	public GImage Level3;
+	public GImage Level4;
+	public GImage Level5;
+	public GImage Level6;
+	public GImage Level7;
+	public GImage Level8;
+	public void run() {
+		Map = new GImage("media/images/Background.png", 0, 0);
+		add(Map);
+	}
 }
