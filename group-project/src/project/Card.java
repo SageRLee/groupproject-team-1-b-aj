ae91906254646e67309e57007d14767687ccdf07
Sage Lee <s_lee146@u.pacific.edu>
2020-11-04 12:18:43 -0800

    Worked on card class

--- group-project/src/project/Card.java
+++ group-project/src/project/Card.java
@@ -1,17 +1,36 @@
 package project;
+
+import acm.graphics.GImage;
+
 public class Card {
 
-	public int attackcards;
-	public int Equipmentcards;
-	public int actioncards;
-	public int manacards;
-	public int healingcards;	
+	
 	//public string getName;
-	
-	private int cost;
-	
-	public int getattackcards() {
-		return attackcards;
+	enum type{
+		ATTACK,
+		EQUIP,
+		ACTION,
+		MANA,
+		HEALING
+		
 	}
+	private int cost;
+	public type cardType;
+	private GImage sprite = new GImage("media/images.Strike.png");
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) { //Probably will be unused, unless we add cards to do that later
+		this.cost = cost;
+	}
+	public boolean canPlay(Entity user, Entity target) {//Function to test if card is playable (can pay mana cost, can target entity, etc)
+		return true;
+	}
+	public void play(Entity user, Entity target) {//Function to play the card
+		
+	}
+	
+	
 }
 
