d910f61e5360f48db641fe4e49690299ab03c9ab
Sage Lee <s_lee146@u.pacific.edu>
2020-11-04 11:49:14 -0800

    Finished base for Mainmenu and graphics

--- group-project/src/project/MainMenu.java
+++ group-project/src/project/MainMenu.java
@@ -3,8 +3,10 @@ package project;
 public class MainMenu {
 	public static final int RESOLUTION_X = 1920;
 	public static final int RESOLUTION_Y = 1080;
+	public static MainMenuGraphics mmGraphics = new MainMenuGraphics();
 	public static void main(String[] args) {
-
+			mmGraphics.start();
+			
 		
 	}
 	
--- group-project/src/project/MainMenuGraphics.java
+++ group-project/src/project/MainMenuGraphics.java
@@ -9,12 +9,19 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class MainMenuGraphics extends ProjectGraphics {
+	//Images
 	private static GImage title = new GImage("media/images/Title.jpg");
 	private static GImage play = new GImage("media/images/PlayButton.png");
 	private static GImage shop = new GImage("media/images/Shop.png");
+	private static GImage charSelect = new GImage("media/images/CharacterSelect.png");
 	private static GImage background = new GImage("media/images/Background.png", 0, 0);//Yeah I know the background is low res, it's just a placeholder 
 	//private Timer someTimeVar = new Timer(33, this);
+	private static GObject target;
 	private GRect hover = new GRect(0, 0, 3, 3 );
+	//Classes
+	private Map mp = new Map();
+	//private Shop shp = new Shop();
+	private CharacterSelect character = new CharacterSelect();
 	public void run() {
 		initializeApplet();
 		addMouseListeners();
@@ -23,7 +30,9 @@ public class MainMenuGraphics extends ProjectGraphics {
 		play.setSize(MainMenu.RESOLUTION_X / 6, MainMenu.RESOLUTION_Y / 6);
 		play.setLocation(MainMenu.RESOLUTION_X / 2 - play.getWidth()/2, MainMenu.RESOLUTION_Y / 3);
 		shop.setSize(MainMenu.RESOLUTION_X / 6, MainMenu.RESOLUTION_Y / 6);
-		shop.setLocation(MainMenu.RESOLUTION_X / 2 - shop.getWidth()/2, MainMenu.RESOLUTION_Y / 2 + 64);
+		shop.setLocation(MainMenu.RESOLUTION_X / 3 - shop.getWidth()/2, MainMenu.RESOLUTION_Y / 2 + 64);
+		charSelect.setSize(MainMenu.RESOLUTION_X / 6, MainMenu.RESOLUTION_Y / 6);
+		charSelect.setLocation(MainMenu.RESOLUTION_X * 2 / 3 - charSelect.getWidth()/2, MainMenu.RESOLUTION_Y / 2 + 64);
 		title.setSize(MainMenu.RESOLUTION_X / 4, MainMenu.RESOLUTION_Y / 6);
 		title.setLocation(MainMenu.RESOLUTION_X / 2 - title.getWidth()/2, MainMenu.RESOLUTION_Y / 8);
 		hover.setSize(MainMenu.RESOLUTION_X / 6 + 16, MainMenu.RESOLUTION_Y / 6 + 16);
@@ -37,21 +46,38 @@ public class MainMenuGraphics extends ProjectGraphics {
 		add(title);
 		add(play);
 		add(shop);
+		add(charSelect);
 	
 		
 	}
 	public void mousePressed(MouseEvent e) {
 		//Mouse event for all button options
-		
-	}
+		if(target == null) {
+			
+		} else if(target == play) {
+			System.out.println("Playing game");	
+			
+			mp.run();
+			
+			} else if(target == shop){
+				System.out.println("Opening shop");
+				//shp.run();
+				
+			} else if(target == charSelect) {
+				System.out.println("Opening character select");
+				//character.run();
+			}
+		}
 	public void mouseMoved(MouseEvent e) {
 		//Mouse Hovering
-		if(getElementAt(e.getX(), e.getY()) == play || getElementAt(e.getX(), e.getY()) == shop) {
+		if(getElementAt(e.getX(), e.getY()) == play || getElementAt(e.getX(), e.getY()) == shop || getElementAt(e.getX(), e.getY()) == charSelect ) {
 			hover.setLocation( getElementAt(e.getX(), e.getY()).getX() - 8,  getElementAt(e.getX(), e.getY()).getY() - 8);
 			hover.setVisible(true);
+			target = getElementAt(e.getX(), e.getY());
 		}
 		else {
 			hover.setVisible(false);
+			target = null;
 		}
 	}
 
