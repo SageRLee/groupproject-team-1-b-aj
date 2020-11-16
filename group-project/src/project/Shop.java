650f1c543cb51344826e1dd84180e65d9a3c55f8
Meier-chen <m_chen27@u.pacific.edu>
2020-11-15 21:59:41 -0800

    hascard code

--- group-project/src/project/Shop.java
+++ group-project/src/project/Shop.java
@@ -6,6 +6,7 @@ public class Shop {
 	private int buycards;
 	private int sellcards;
 	  public int Card;
+	private String cardname;
 	
 	public Shop( int buycards, int sellcards, int Card) {
 		this.buycards = buycards;
@@ -21,5 +22,13 @@ public class Shop {
 	   return sellcards;
 }
 
+    public void hasCards( String cardname,String cost){  
+        this.cardname = cardname;
 
+        if(cardname == "Attack" || cardname == "Mana" || cardname == "Equipment"||cardname == "Action")
+        {
+            this.cardname = cost;
+        }
+    } 
+    
 }
