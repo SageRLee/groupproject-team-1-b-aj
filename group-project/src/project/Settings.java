c3bada65484e5775dcc7a7ab5d1375b9441cc1ab
Sage Lee <s_lee146@u.pacific.edu>
2020-11-21 19:53:06 -0800

    Fixed charselect bug

--- group-project/src/project/Settings.java
+++ group-project/src/project/Settings.java
@@ -18,24 +18,27 @@ public class Settings {
 		this.enabled = enabled;
 	}
 	private GRect blackscrn = new GRect(0, 0, MainMenu.RESOLUTION_X, MainMenu.RESOLUTION_Y);
+	private GRect menu = new GRect(0, 0, MainMenu.RESOLUTION_X, MainMenu.RESOLUTION_Y);
 	MainMenu program;
 	public Settings(MainMenu program, Player player){
 		this.program = program;
 		blackscrn.setFillColor(new Color(0, 0, 0, 125));
 		blackscrn.setFilled(true);
-		program.add(blackscrn);
+		menu.setLocation(MainMenu.RESOLUTION_X / 4, MainMenu.RESOLUTION_Y / 6);
+		menu.setSize(MainMenu.RESOLUTION_X * 0.4, MainMenu.RESOLUTION_Y * 0.8);
+		menu.setFilled(true);
 	}
 	public void openSettings(){
-		System.out.println("happens");
-		blackscrn.setVisible(true);
+		program.add(blackscrn);
+		program.add(menu);
 		enabled = true;
-		blackscrn.sendToFront();
+	
 
 	}
 	public void closeSettings() {
 		enabled = false;
-		blackscrn.setVisible(false);
-		blackscrn.sendToBack();
-		
+		program.remove(blackscrn);	
+		program.remove(menu);
+
 	};
 }
