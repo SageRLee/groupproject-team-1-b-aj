6f38a80c543ada8a280214fbdcf74c78a27ae464
Sage Lee <s_lee146@u.pacific.edu>
2020-11-27 12:27:09 -0800

    Test push on new computer

--- group-project/src/project/Settings.java
+++ group-project/src/project/Settings.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyEvent;
 import acm.graphics.GRect;
 import project.MainMenu;
 import project.Player;
-
+//Test
 public class Settings {
 	private boolean enabled = false;
 	private String option[][] = {
